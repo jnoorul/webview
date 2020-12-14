f2bfad35a2de36d56a5c84a436bfb1bb5bc6da94
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2020-12-14 17:43:51 +0800

    initial web version

---
+++ webview-web/src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <UniversalLink to="portfolio">Portfolio</UniversalLink>
+            </li>
+            <li>
+              <UniversalLink to="insights">Insights</UniversalLink>
+            </li>
+            <li>
+              <UniversalLink to="instruments">Instruments</UniversalLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/instruments">
+            <Instruments />
+          </Route>
+          <Route path="/insights">
+            <Insights />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Portfolio() {
+  return <h2>Portfolio</h2>;
+}
+
+function Insights() {
+  return <h2>Insights</h2>;
+}
+
+function Instruments() {
+  return <h2>Instruments</h2>;
+}
+
+function UniversalLink({ to = "", children = "" }) {
+  const [isApp, setIsApp] = useState(false);
+
+  useEffect(() => {
+    var userAgent = window.navigator.userAgent.toLowerCase();
+    setIsApp(/iphone|ipod|ipad/.test(userAgent));
+  }, [isApp]);
+
+  if (isApp) {
+    return <a href={`dpb://home?page=${to}`}>{children}</a>;
+  }
+  return <Link to={to}>{children}</Link>;
+}
